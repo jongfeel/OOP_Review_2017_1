5eb847c872b684a527461c272679fab2518d054d
Kim Jong Feel <DeveloperFEEL@gmail.com>
2016-12-09 14:50:28 +0900

    value and object first commit

--- OOP_Review_2017_1/OOP_Review_2017_1/Program.cs
+++ OOP_Review_2017_1/OOP_Review_2017_1/Program.cs
@@ -10,6 +10,28 @@ namespace OOP_Review_2017_1
     {
         static void Main(string[] args)
         {
+            Test t1 = new Test();
+            Test t2 = new Test();
+            
+            // why?
+            bool resultEquals = object.Equals(t1, t2);  // false
+            bool resultReferenceEquals = object.ReferenceEquals(t1, t2); // false
+
+            string writeformat = "{0}, {1}";
+            Console.WriteLine(writeformat, "object.Equals(t1, t2)", resultEquals);
+            Console.WriteLine(writeformat, "object.ReferenceEquals(t1, t2)", resultReferenceEquals);
+
+            t1.Id = 1;
+            t2.Id = 1;
+
+            resultEquals = object.Equals(t1.Id, t2.Id);  // true
+            resultReferenceEquals = object.ReferenceEquals(t1.Id, t2.Id); // false, why???
+            
+            Console.WriteLine(writeformat, "object.Equals(t1.Id, t2.Id)", resultEquals);
+            Console.WriteLine(writeformat, "object.ReferenceEquals(t1.Id, t2.Id)", resultReferenceEquals);
+
+            // Diffenrent "value" and "object"
+            // Let's check out!
         }
     }
 }
