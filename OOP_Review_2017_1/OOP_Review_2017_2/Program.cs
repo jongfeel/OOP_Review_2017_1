b40905213f506b3a642d4d4adde12fc360d4a9fb
Kim Jong Feel <DeveloperFEEL@gmail.com>
2017-01-08 11:50:11 +0900

    Add files, OOP review2

---
+++ OOP_Review_2017_1/OOP_Review_2017_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Review_2017_2
+{
+    class Program
+    {
+        // Declare delegate 
+        public delegate void DelegateAction();
+        // Difference DelegateAction and DelegateActionMethod
+        public static void DelegateActionMethod()
+        {
+            Console.WriteLine("Called DelegateActionMethod!");
+        }
+
+        static void Main(string[] args)
+        {
+            // Create delegate object
+            // DelegateAction parameter: intellisense "void () target", what is mean?
+            DelegateAction da = new DelegateAction(DelegateActionMethod);
+            da.Invoke();
+
+            // Lambda
+            DelegateAction da2 = () => Console.WriteLine("Called da2 delegate variable.");
+
+        }
+    }
+}
