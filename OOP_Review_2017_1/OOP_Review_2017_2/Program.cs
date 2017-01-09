1baf30e5d2a95bc34862727419887d7b0591e5d1
Kim Jong Feel <DeveloperFEEL@gmail.com>
2017-01-09 17:31:11 +0900

    add implement yield keyword

--- OOP_Review_2017_1/OOP_Review_2017_2/Program.cs
+++ OOP_Review_2017_1/OOP_Review_2017_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,6 +27,26 @@ namespace OOP_Review_2017_2
             // Lambda
             DelegateAction da2 = () => Console.WriteLine("Called da2 delegate variable.");
 
+            // yield
+            foreach (var item in GetNames)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine("AOA member: " + item);
+                }
+            }
+        }
+
+        public static IEnumerable GetNames
+        {
+            get
+            {
+                yield return "Seolhyun";
+                yield return null;
+                yield return "Cho-a";
+                yield break;
+                // yield return "Chou Tzu-yu"; // no meaning
+            }
         }
     }
 }
