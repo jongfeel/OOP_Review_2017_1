63559298ab52178fdd448977e706753605f79a94
Kim Jong Feel <DeveloperFEEL@gmail.com>
2017-02-03 11:32:29 +0900

    Add attribute code and TestProject

--- OOP_Review_2017_1/OOP_Review_2017_4/Program.cs
+++ OOP_Review_2017_1/OOP_Review_2017_4/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOP_Review_2017_4
 {
+    class MyDefaultValueAttribute : Attribute
+    {
+        public int Value { get; set; }
+
+        public MyDefaultValueAttribute(int value)
+        {
+            Value = value;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -100,6 +113,51 @@ namespace OOP_Review_2017_4
             result = returnValue == true ? "Win!" : "Fail!";
             Console.WriteLine(result);
             #endregion
+
+            #region Attribute
+            // Attribute(애트리뷰트)는 Attribute abstract class를 상속받아 구현하는 클래스들로
+            // 특정 조건에 한해서 동작하도록 특성, 상태를 부여한다.
+            Console.WriteLine();
+            Console.WriteLine("Attribute phase");
+
+            // #define DEBUG라고 정의해준 것과 동일
+            CallDebugMode("debugging!");
+
+            // DllImport
+            // dll 안에 static method에 진입하도록 특성 부여하고 실제 호출 가능하게 만든다.
+            // options
+            // IconInformation = 0x000040
+            // YesNo              = 0x000004
+            //MessageBox(IntPtr.Zero, "message box called", "Info", 0x000040 | 0x000004);
+
+            // Visual studio에 특별히 warning setting을 건들지 않았다면 경고로 표시해줌.
+            //Delete(0);
+
+            // Attribute는 왜 쓰는 것일까???
+            // 동적인 로직 처리가 아닌 정적인 특성, 상태 자체를 표현해 주고 싶은데 (전문 용어로 metadata)
+            // text base인 comment로는 충분하지 않기 때문이다.
+
+            // 그럼 동작 원리는???
+            // 눈치 빠른 분은 아셨겠지만 reflection을 통해 동작하게 만드는 거죠.
+
+            // custom attribute 만들어서 reflection을 통해 알아보기
+            
+            #endregion
+        }
+
+        [Conditional("DEBUG")]
+        public static void CallDebugMode(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
+        [Obsolete("This method is deprecated.")]
+        public static void Delete(int value)
+        {
+            // Unity에서 많이 봤던 그 attribute
         }
     }
 }
