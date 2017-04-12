939e851ec176dec689d5f0dab11c383e2db00443
Kim Jong Feel <DeveloperFEEL@gmail.com>
2017-04-12 10:40:50 +0900

    oop review 1-12

---
+++ OOP_Review_2017_1/OOP_Review_2017_12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Review_2017_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code reivew, WPF MVVM
+            // 1. 김한별
+            // 학교 연구과제중 WPF, MVVM을 쓰면 좋지 않을까에 대한 리뷰
+            // https://github.com/progsecu/Project-Kinect// 다음 시간
+
+
+            // 1. Kyung Wong Gil님의
+            // Synchronous, Asynchronous, thread and callback, APM(Asynchronous Programming Model)
+            // 2. Iccen님의 opencv를 이용해서 어떤 특정 포인트 찾아내기 리뷰 (선 내용 공유)
+
+
+        }
+    }
+}
